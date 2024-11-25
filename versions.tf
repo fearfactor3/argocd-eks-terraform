d742b56169f98e74ff320d9284a1024e1ffb7146
Anthony Major <wayne.major03@gmail.com>
2024-11-25 15:31:04 -0500

    Removed multiple versions.tf files to make it more maintainable.

--- versions.tf
+++ versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.8"
@@ -9,6 +13,5 @@ terraform {
       version = "~> 2.18"
     }
   }
-
   required_version = ">= 1.4.0"
 }
